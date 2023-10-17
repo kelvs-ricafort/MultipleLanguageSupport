038fc729a54bd66d776c4cf62bc813e6a5e2828b
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-17 15:01:24 +0800

    Multi Language Support

--- app/src/main/java/com/newtechieblog/wordpress/views/multiplelanguagesupport/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/multiplelanguagesupport/MainActivity.java
@@ -3,12 +3,21 @@ package com.newtechieblog.wordpress.views.multiplelanguagesupport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShow = findViewById(R.id.buttonToast);
+
+        btnShow.setOnClickListener(v -> Toast.makeText(getApplicationContext(), R.string.toast, Toast.LENGTH_LONG).show());
+
     }
 }
